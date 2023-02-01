35407e52941560dc9593172b03152bae85dafdce
guandong <1623088350@qq.com>
2023-02-01 10:26:34 +0800

    add cpp test

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 
 相应的 dll 代码在 项目 node-14.21.1
+
+
+
+C++ 调用 node.dll 测试代码在 testdll.cpp
---
+++ testdll.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+#include "windows.h"
+
+int main()
+{
+    HINSTANCE   ghDLL = NULL;
+    ghDLL = LoadLibrary("D:\\GitHub\\node-14.21.1\\out\\Debug\\node.dll");
+
+    typedef int (_cdecl* FunctionPtr) (int argc, wchar_t* wargv[]);
+
+    FunctionPtr wmain;
+
+    wmain = (FunctionPtr)GetProcAddress(ghDLL, "wmain");
+
+    int argc = 2;
+
+    wchar_t* wargv[] = {
+      (wchar_t*)L"C:\\projects\\edge-js\\tools\\build\\node-14.21.1\\out\\Debug\\node2.exe",
+      //(wchar_t*)L"C:\\projects\\edge-js\\tools\\build\\node-14.21.1\\out\\Debug\\pmserver\\server.js",
+      (wchar_t*)L"D:\\GitHub\\echodict\\pmserver\\server.js"
+    };
+
+    wmain(argc, wargv);
+
+    std::cout << "Hello World!\n";
+}
